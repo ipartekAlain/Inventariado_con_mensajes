c7f5773dea95107bcbfbc553d2d56d103e4cc3a9
Alain <107689010+ipartekAlain@users.noreply.github.com>
2024-02-05 10:09:50 +0100

    creamos la clase productoService
    creamos la clase para escribir todas y cada una de las funciones que acceden al repository

---
+++ src/main/java/com/ipartek/service/ProductoService.java
@@ -0,0 +1,11 @@
+package com.ipartek.service;
+
+public class ProductoService {
+
+	
+	//hola, esta clase que he creado esta en mi pc solamente
+	
+	//si la quiero subir, tendre que hacer un pull
+	
+	
+}
